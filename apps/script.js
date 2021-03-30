fb8e49cd063770ba3bd261e57fb039df480cae01
Vjekoni <dizdar.vjeko@gmail.com>
2021-03-30 21:56:56 +0200

    fixed some issues
    deleted extra nav, made some changes.

--- apps/script.js
+++ apps/script.js
@@ -1,24 +1,26 @@
-const openBtn = document.querySelector("#open-btn");
-const closeBtn = document.querySelector("#close-btn");
-const menu = document.querySelector(".header__menu");
+const openBtn = document.querySelector("#menu-btn-open");
+const closeBtn = document.querySelector("#menu-btn-close");
+const menu = document.querySelector(".header__navbar");
 const body = document.querySelector("#body");
-const linkItems = document.querySelectorAll(".toggle");
+const menuItems = document.querySelectorAll(".toggle");
 
 openBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", closeMenu);
 
-linkItems.forEach(item => {
+
+
+menuItems.forEach(item => {
       item.addEventListener("click", closeMenu);
 });
 
 
 function openMenu() {
-      menu.classList.add("show")
+      menu.classList.add("show");
       body.style.overflow = "hidden";
 }
 
 function closeMenu() {
-      menu.classList.remove("show")
-      body.style.overflowX = "hidden";
+      menu.classList.remove("show");
       body.style.overflowY = "auto";
+      body.style.overflowX = "hidden";
 }
